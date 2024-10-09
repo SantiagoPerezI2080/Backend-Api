3c975284b6654f3d057fb637ab7cad3291d8b49f
SantiagoPerezI2080 <fredy.perez.i@uniautonoma.edu.co>
2024-10-08 19:46:14 -0500

    Mi primer commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Api\ProductoApiController;
+use App\Http\Controllers\Api\TareaApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Rutas productos
+Route::get('productos', [ProductoApiController::class, 'index']);
+Route::post('productos', [ProductoApiController::class,'store']);
+Route::get('productos/{id}', [ProductoApiController::class, 'show']);
+Route::put('productos/{id}', [ProductoApiController::class, 'update']);
+Route::delete('productos/{id}', [ProductoApiController::class, 'destroy']);
+
+// Rutas tareas
+Route::get('tareas', [TareaApiController::class, 'index']);
+Route::post('tareas', [TareaApiController::class,'store']);
+Route::get('tareas/{id}', [TareaApiController::class, 'show']);
+Route::put('tareas/{id}', [TareaApiController::class, 'update']);
+Route::delete('tareas/{id}', [TareaApiController::class, 'destroy']);
